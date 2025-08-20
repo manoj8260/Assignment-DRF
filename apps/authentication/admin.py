361d956695d8b6cf59719d845d0164be538a4a1d
manoj8260 <mn047653@gmail.com>
2025-08-21 02:03:19 +0530

    Add .gitignore, remove pycache, update user model & admin

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class UserModelAdmin(UserAdmin):
+    model = User
+    list_display = ['id','email','is_active','is_staff','is_superuser']
+    list_filter=['is_superuser']
+    search_fields = ('email','first_name')
+    ordering = ('email','id')
+    filter_horizontal = ('groups', 'user_permissions') 
+    readonly_fields = ('date_joined', 'date_updated')
+    fieldsets = (
+        ('User Credentials', {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name','last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions' )}),
+        ('Important dates', {'fields': ('last_login','date_joined', 'date_updated' )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User,UserModelAdmin)    
